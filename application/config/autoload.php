3307379facedce2116564f8ec94108e3fb8bff46
Stanislas-Web <stamislasmakengo1@gmail.com>
2023-07-16 22:01:20 +0100

    update code

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -90,7 +90,7 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 $autoload['helper'] = array();
-$autoload['model'] = array('autoload_models');
+// $autoload['model'] = array('autoload_models');
 
 /*
 | -------------------------------------------------------------------
--- application/controllers/authentification/Inscription.php
+++ application/controllers/authentification/Inscription.php
@@ -8,13 +8,13 @@ class Inscription extends CI_Controller {
 		$this->load->view('authentification/inscription');
 	}
 
-    public function registration_form(){
-        $this->load->model('Autoload_models_model');
-    }
+    // public function registration_form(){
+    //     $this->load->model('Autoload_models_model');
+    // }
 
-    public function login_form(){
-        $this-> authentification_model->login_user();
-    }
+    // public function login_form(){
+    //     $this-> authentification_model->login_user();
+    // }
     
 
 }
