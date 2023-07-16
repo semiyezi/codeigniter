2dae572a8483d907dc92a404fed7e4d1930e509e
Stanislas-Web <stamislasmakengo1@gmail.com>
2023-07-16 23:56:16 +0100

    update code

--- application/controllers/authentification/Inscription.php
+++ application/controllers/authentification/Inscription.php
@@ -11,8 +11,10 @@ class Inscription extends CI_Controller {
 	public function registration_form(){
         $this->load->model('authentification_model');
 		$this->authentification_model->register_user();
+        // $id = 123;
+        redirect(base_url('index.php/authentification/connexion/index'));
 	}
-    
+
 
 	public function login_form(){
 		$this->authentification_model->login_user();
